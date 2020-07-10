a5dc0578cd88d74be190604be3e0b1602c0371e7
Lauren West <laurenwest12@gmail.com>
2020-07-10 15:56:58 -0400

    updated single cart item

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { removeAllItemsFromCartThunk } from '../redux/actions/cart'
-import { checkoutAsUserThunk } from '../redux/actions/shared'
-import { makePriceCurrencyFormat } from '../HelperFunctions'
-import SingleCartItem from './SingleCartItem'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { removeAllItemsFromCartThunk } from '../redux/actions/cart';
+import { checkoutAsUserThunk } from '../redux/actions/shared';
+import { makePriceCurrencyFormat } from '../HelperFunctions';
+import SingleCartItem from './SingleCartItem';
 
 class Cart extends Component {
   render() {
@@ -15,103 +15,115 @@ class Cart extends Component {
       removeAllItemsFromCart,
       history,
       products,
-      checkoutAsUser
-    } = this.props
+      checkoutAsUser,
+    } = this.props;
     return (
-      <div className="cart-list">
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <div className="row">
-              <span className="col-sm-8" />
-              <span className="col-sm-2 text-right">Quantity</span>
-              <span className="col-sm-2 text-right">Price</span>
-            </div>
-          </li>
-          {cart.map(item => (
+      <div className="cart">
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+          {cart.map((item) => (
             <SingleCartItem cartItem={item} key={item.id} history={history} />
           ))}
-        </ul>
-        <div className="row cart-summary">
-          <span className="col-sm-8" />
-          <span className="col-sm-2 text-right">
-            <strong>Total Price</strong>
-          </span>
-          <span className="col-sm-2 price-color text-right">
-            {makePriceCurrencyFormat(totalCartPrice)}
-          </span>
-        </div>
-
-        <div className="d-flex justify-content-between">
-          <button
-            type="button"
-            className="remove-btn"
-            onClick={() => removeAllItemsFromCart(user.id, currentOrder.id)}
-          >
-            Clear Cart
-          </button>
-
-          <button
-            type="button"
-            onClick={() =>
-              checkoutAsUser(user, currentOrder, cart, products, history)
-            }
-            className="standard-btn"
-            disabled={
-              cart.length === 0 || cart.some(item => item.canNotBeCheckedOut)
-            }
-          >
-            Checkout
-          </button>
-        </div>
+        </table>
       </div>
-    )
+      // <div className="cart-list">
+      //   <ul className="list-group list-group-flush">
+      //     <li className="list-group-item">
+      //       <div className="row">
+      //         <span className="col-sm-8" />
+      //         <span className="col-sm-2 text-right">Quantity</span>
+      //         <span className="col-sm-2 text-right">Price</span>
+      //       </div>
+      //     </li>
+      //     {cart.map(item => (
+      //       <SingleCartItem cartItem={item} key={item.id} history={history} />
+      //     ))}
+      //   </ul>
+      //   <div className="row cart-summary">
+      //     <span className="col-sm-8" />
+      //     <span className="col-sm-2 text-right">
+      //       <strong>Total Price</strong>
+      //     </span>
+      //     <span className="col-sm-2 price-color text-right">
+      //       {makePriceCurrencyFormat(totalCartPrice)}
+      //     </span>
+      //   </div>
+
+      //   <div className="d-flex justify-content-between">
+      //     <button
+      //       type="button"
+      //       className="remove-btn"
+      //       onClick={() => removeAllItemsFromCart(user.id, currentOrder.id)}
+      //     >
+      //       Clear Cart
+      //     </button>
+
+      //     <button
+      //       type="button"
+      //       onClick={() =>
+      //         checkoutAsUser(user, currentOrder, cart, products, history)
+      //       }
+      //       className="standard-btn"
+      //       disabled={
+      //         cart.length === 0 || cart.some(item => item.canNotBeCheckedOut)
+      //       }
+      //     >
+      //       Checkout
+      //     </button>
+      //   </div>
+      // </div>
+    );
   }
 }
 
 const mapStateToProps = ({ loggedInUser, userOrders, products, cart }) => {
   const productsMap = products.reduce((acc, product) => {
-    const { name, price, inventoryQuantity, image } = product
-    const selectedProductFields = { name, price, inventoryQuantity, image }
-    acc[`productId${product.id}`] = selectedProductFields
-    return acc
-  }, {})
+    const { name, price, inventoryQuantity, image } = product;
+    const selectedProductFields = { name, price, inventoryQuantity, image };
+    acc[`productId${product.id}`] = selectedProductFields;
+    return acc;
+  }, {});
   const cartTransformed = cart
-    .map(item => ({
+    .map((item) => ({
       ...item,
-      ...productsMap[`productId${item.productId}`]
+      ...productsMap[`productId${item.productId}`],
     }))
-    .map(item => ({
+    .map((item) => ({
       ...item,
       totalItemPrice: (item.quantity * item.price).toFixed(2),
       canNotBeCheckedOut:
         ['', '0'].includes(item.quantity) ||
-        item.inventoryQuantity - item.quantity < 0
-    }))
+        item.inventoryQuantity - item.quantity < 0,
+    }));
   const totalCartPrice = cartTransformed[0]
     ? cartTransformed.reduce(
         (acc, item) => acc + Number(item.totalItemPrice),
         0
       )
-    : 0
+    : 0;
   return {
     products,
     cart: cartTransformed,
     totalCartPrice,
     user: loggedInUser,
-    currentOrder: userOrders.find(order => order.status === 'cart') || {}
-  }
-}
+    currentOrder: userOrders.find((order) => order.status === 'cart') || {},
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     removeAllItemsFromCart: (userId, orderId) =>
       dispatch(removeAllItemsFromCartThunk(userId, orderId)),
     checkoutAsUser: (user, currentOrder, cart, products, history) =>
-      dispatch(checkoutAsUserThunk(user, currentOrder, cart, products, history))
-  }
-}
+      dispatch(
+        checkoutAsUserThunk(user, currentOrder, cart, products, history)
+      ),
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Cart)
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- client/components/SingleCartItem.js
+++ client/components/SingleCartItem.js
@@ -1,63 +1,61 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { removeFromCartThunk } from '../redux/actions/cart'
-import { makePriceCurrencyFormat } from '../HelperFunctions'
-import CartQuantitySelector from './CartQuantitySelector'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { removeFromCartThunk } from '../redux/actions/cart';
+import { makePriceCurrencyFormat } from '../HelperFunctions';
+import CartQuantitySelector from './CartQuantitySelector';
 
 class SingleCartItem extends Component {
   render() {
-    const { cartItem, userId, orderId, removeFromCart, history } = this.props
-    const { id, name, productId, totalItemPrice, image } = cartItem
+    const { cartItem, userId, orderId, removeFromCart, history } = this.props;
+    const { id, name, productId, totalItemPrice, image } = cartItem;
     return (
-      <li key={id} className="list-group-item">
-        <div className="row">
-          <span className="col-sm-2">
-            <img
-              src={image}
-              className="cart-item-image"
-              onClick={() => history.push(`/products/${productId}`)}
-            />
-          </span>
+      <tr>
+        <td>
+          <img
+            src={image}
+            className="cart-item-image"
+            onClick={() => history.push(`/products/${productId}`)}
+          />
+        </td>
+        <td>
           <Link to={`/products/${productId}`} className="col-sm-6">
             {name}
-          </Link>{' '}
+          </Link>
+        </td>
+        <td>
           <CartQuantitySelector userId={userId} cartItem={cartItem} />
-          <span className="col-sm-2 price-color text-right">
-            {makePriceCurrencyFormat(totalItemPrice)}
-          </span>
-        </div>
-
-        <button
-          type="button"
-          className="remove-btn"
-          onClick={() => {
-            removeFromCart(userId, orderId, id)
-          }}
-        >
-          Remove item from cart
-        </button>
-      </li>
-    )
+        </td>
+        <td>{makePriceCurrencyFormat(totalItemPrice)}</td>
+        <td>
+          <button
+            type="button"
+            className="remove-btn"
+            onClick={() => {
+              removeFromCart(userId, orderId, id);
+            }}
+          >
+            X
+          </button>
+        </td>
+      </tr>
+    );
   }
 }
 
 const mapStateToProps = ({ loggedInUser, userOrders }) => {
-  const cartOrder = userOrders.find(order => order.status === 'cart')
+  const cartOrder = userOrders.find((order) => order.status === 'cart');
   return {
     userId: loggedInUser.id,
-    orderId: cartOrder && cartOrder.id
-  }
-}
+    orderId: cartOrder && cartOrder.id,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     removeFromCart: (userId, orderId, lineitemId) =>
-      dispatch(removeFromCartThunk(userId, orderId, lineitemId))
-  }
-}
+      dispatch(removeFromCartThunk(userId, orderId, lineitemId)),
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SingleCartItem)
+export default connect(mapStateToProps, mapDispatchToProps)(SingleCartItem);
